8837de4a22e31e62e084c03014d18aa316868621
thelamest <thelamest@las>
2015-01-11 17:29:23 +0100

    adjusted sandmonster spawn params to higher light and density

--- mods/mobs/sandmonster.lua
+++ mods/mobs/sandmonster.lua
@@ -64,4 +64,4 @@ mobs:register_mob("mobs:sand_monster", {
 	step = 0.5,
 	blood_texture = "mobs_blood.png",
 })
-mobs:register_spawn("mobs:sand_monster", {"default:stone","default:desert_sand", "default:sand"}, 8, -1, 700, 1, 31000)
+mobs:register_spawn("mobs:sand_monster", {"default:stone","default:desert_sand", "default:sand"}, 15, -1, 700, 10, 31000)
