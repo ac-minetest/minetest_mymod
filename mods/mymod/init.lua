8331c9f51b368162d3c4571c122c84e9f700a858
thelamest <minetest@swiado.me>
2015-02-05 18:30:23 +0100

    sun burns away from spawn - don't affect admins

--- mods/mymod/init.lua
+++ mods/mymod/init.lua
@@ -259,11 +259,11 @@ minetest.register_globalstep(function(dtime)
 			
 			-- SURVIVABILITY CHECK
 			
-			if pos.y>0 and dist>500 and playerdata[name].xp<1000 then
+			if pos.y>0 and dist>500 and playerdata[name].xp<1000 and privs["privs"] == false then
 				if minetest.get_node_light(pos) ~= nil then -- crashed once, safety
 				if minetest.get_node_light(pos)>LIGHT_MAX*0.9 then
 					if player:get_hp()==20 then
-						minetest.chat_send_player(name,"You are exhausted from the sun, find shelter or get at least 1000 experience or return closer to spawn.")
+						minetest.chat_send_player(name,"Desert sunlight is scorching hot. Find shadow, get at least 1000 experience or return closer to spawn.")
 					end
 					player:set_hp(player:get_hp()-0.25)
 				end
