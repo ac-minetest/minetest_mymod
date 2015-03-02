701d7ad1f40d42ec37882ccc947b049bdccd7fab
rnd <rnd@rnd.com>
2015-03-02 23:21:19 +0100

    disabled "shared" cheset, use mailbox for that

--- mods/mymod/tweaks.lua
+++ mods/mymod/tweaks.lua
@@ -14,17 +14,19 @@
 -- overwrite("default:sapling")
 -- overwrite("default:torch")
 
-local function shared_chests()
-	local name = "default:chest";
-	local table = minetest.registered_nodes[name];
-	local table2 = {};
-	for i,v in pairs(table) do table2[i] = v end
-	table2.allow_metadata_inventory_take = function(pos, listname, index, stack, player) -- rnd: make chests inside protection zone protected against take
-		if not protector.can_dig(5,pos,player) then
-			return 0
-		end
-		return stack:get_count()
-	end
-	minetest.register_node(":"..name, table2)	
-end
-shared_chests();
+
+-- local function shared_chests()
+	-- local name = "default:chest";
+	-- local table = minetest.registered_nodes[name];
+	-- local table2 = {};
+	-- for i,v in pairs(table) do table2[i] = v end
+	-- table2.allow_metadata_inventory_take = function(pos, listname, index, stack, player) -- rnd: make chests inside protection zone protected against take
+		
+		-- if not protector.can_dig(5,pos,player) then
+			-- return 0
+		-- end
+		-- return stack:get_count()
+	-- end
+	-- minetest.register_node(":"..name, table2)	
+-- end
+-- shared_chests();
