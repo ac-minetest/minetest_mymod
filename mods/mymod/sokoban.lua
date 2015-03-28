f4fcd0cb7b63fec3124ced3c70873039b6f43a30
rnd <rnd@rnd.com>
2015-03-28 16:10:44 +0100

    ..

--- mods/mymod/sokoban.lua
+++ mods/mymod/sokoban.lua
@@ -188,7 +188,7 @@ local function draw_board() -- pos is bottom left position of checkerboard
 				else
 				if node~="mymod:board_white" then minetest.set_node({x=pos.x+i-1,y=pos.y,z=pos.z+j-1},{name = "mymod:board_white"}) end
 			end
-			node = minetest.get_node({x=pos.x+i-1,y=pos.y+1,z=pos.z-1}).name;
+			node = minetest.get_node({x=pos.x+i-1,y=pos.y+1,z=pos.z+j-1}).name;
 			if node~="air" then minetest.set_node({x=pos.x+i-1,y=pos.y+1,z=pos.z+j-1},{name = "air"}) end
 		end
 	end
